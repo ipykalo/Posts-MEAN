10f3b2b1426f6bacdfaf8c68e0c298be31a8ef4f
Ivan Pykalo <ipyka@softserveinc.com>
2021-04-28 16:05:10 +0300

    small refactoring;

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
 import { PostService } from '../post.service';
@@ -10,22 +11,32 @@ import { PostService } from '../post.service';
     styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-    private destroy: Subscription;
+    private destroy: Subscription[] = [];
     posts: Post[] = [];
 
-    constructor(private postService: PostService) { }
+    constructor(private postService: PostService, private router: Router) { }
 
     ngOnInit(): void {
-        this.postService.fetchPosts();
-        this.destroy = this.postService.getPostsSub()
-            .subscribe((resp: Post[]) => this.posts = resp);
+        this.fetchPost();
     }
 
     ngOnDestroy(): void {
-        this.destroy.unsubscribe();
+        this.destroy?.forEach((sub: Subscription) => sub?.unsubscribe());
     }
 
     onDelete(post: Post): void {
-        this.postService.deletePost(post._id);
+        const sub: Subscription = this.postService.deletePost(post._id)
+            .subscribe(resp => {
+                if (resp?.message) {
+                    this.fetchPost();
+                }
+            });
+        this.destroy.push(sub);
+    }
+
+    private fetchPost(): void {
+        const sub: Subscription = this.postService.getPosts()
+            .subscribe((resp: Post[]) => this.posts = resp);
+        this.destroy.push(sub);
     }
 }
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { Observable, Subject } from "rxjs";
+import { Observable, of } from "rxjs";
+import { map } from "rxjs/operators";
 import { Post } from "./post.model";
 import { HttpClient } from "@angular/common/http";
 
@@ -9,45 +10,38 @@ const URL: string = 'http://localhost:3000/api/posts';
     providedIn: 'root'
 })
 export class PostService {
-    private sub: Subject<Post[]> = new Subject();
     private posts: Post[];
 
     constructor(private http: HttpClient) { }
 
-    getPostsSub(): Observable<Post[]> {
-        return this.sub.asObservable();
-    }
-
-    addPost(post: Post): Observable<{ message: string }> {
-        return this.http.post<{ message: string }>(URL, post)
-    }
-
-    deletePost(id: string): void {
-        this.http.delete<{ message: string }>(`${URL}/${id}`)
-            .subscribe(() => {
-                this.fetchPosts();
-            });
-    }
-
-    updatePost(post: Post): Observable<{ message: string }> {
-        return this.http.put<{ message: string }>(`${URL}/${post._id}`, post);
+    getPosts(): Observable<Post[]> {
+        return this.fetchPosts();
     }
 
     getPost(id: string): Post {
         return this.posts?.find((post: Post) => post?._id === id);
     }
 
+    addPost(post: Post): Observable<{ message: string }> {
+        return this.http.post<{ message: string }>(URL, post)
+    }
+
+    deletePost(id: string): Observable<{ message: string }> {
+        return this.http.delete<{ message: string }>(`${URL}/${id}`);
+    }
+
+    updatePost(post: Post): Observable<{ message: string }> {
+        return this.http.put<{ message: string }>(`${URL}/${post._id}`, post);
+    }
+
     fetchPost(id: string): Observable<Post> {
         return this.http.get<Post>(`${URL}/${id}`);
     }
 
-    fetchPosts(): void {
-        this.http.get<{ message: string, posts: Post[] }>(URL)
-            .subscribe(resp => {
-                if (resp?.posts) {
-                    this.posts = resp.posts;
-                    this.sub.next(resp.posts);
-                }
-            });
+    private fetchPosts(): Observable<Post[]> {
+        return this.http.get<{ message: string, posts: Post[] }>(URL)
+            .pipe(
+                map(resp => this.posts = resp?.posts)
+            );
     }
 }
