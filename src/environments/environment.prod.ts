92bebffcb393a48630594ced6dcc3d422378a551
Ivan Pykalo <ipyka@softserveinc.com>
2021-06-14 13:42:56 +0300

    fixed prodaction url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://posts-env.eba-zwk4sjex.eu-west-1.elasticbeanstalk.com/'
+    baseUrl: 'http://posts-env.eba-zwk4sjex.eu-west-1.elasticbeanstalk.com'
 };
 
 export const URLS = {
